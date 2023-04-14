10e00c22f66a3bff33c86ad8c6df854f9d714c95
Vitor Arthur <61673650+arthvitor@users.noreply.github.com>
2023-04-14 14:31:23 -0300

    Add files via upload

--- bntc_weaver.py
+++ bntc_weaver.py
@@ -1,11 +1,13 @@
 # benestic weaver
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 
 # importando bibliotecas
 import requests
 import gspread
 from flask import request
 from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from urllib.request import urlopen
 
 
 # FUNÇÕES PARA RECEBER DADOS DO SPOTIFY
@@ -59,6 +61,101 @@ def get_release(country=str, limit=int, header_access=dict):
     return result
 
 
+# função para recomendação do dia
+def recommend_noti(header_access, seed_artist, seed_genres):
+    '''
+    Função retorna uma recomendação de música, dependendo do gênero e de um outro artista
+    :param header_access: [DICT] header de autentificação
+    :param seed_artist: [STR] Nome do artista base
+    :param seed_genres: [STR] Gênero para base da busca
+    :return str musica:
+    '''
+
+    # acumulador de singles
+    list_singles = []
+    artist = get_search(seed_artist, ['artist'], limit=1, header_access=header_access)
+    artist_id = artist['artists']['items'][0]['id']
+
+    # query da requsição
+    query_data = {
+    'limit': 100,
+    'seed_artist': [artist_id],
+    'seed_genres': [seed_genres]}
+    recommend_not = requests.get(f'https://api.spotify.com/v1/recommendations', headers=header_access, params=query_data).json()
+
+    # adicionando id de singles ao acumulador
+    for n in recommend_not['tracks']:
+        if n['album']['album_group'] == 'SINGLE':
+            list_singles.append(n['album']['id'])
+        
+    id=list_singles[0]
+    musica = requests.get(f'https://api.spotify.com/v1/albums/{id}', headers=header_access).json()
+    return musica
+
+
+# FUNÇÃO PARA CRIAÇÃO DE IMAGEM
+def make_release_image(url=str):
+    '''
+    Função retorna imagem de capa do último lançamento
+    :param url: [STR] URL da imagem do Spotify que será usada como capa
+    :return PIL.Image.Image out:
+    '''
+
+    # gerando sobreposição da mascara com imagem do spotify
+    url = url
+    im1 = Image.open('lancamento.png').convert('RGBA')
+    im2 = Image.open(urlopen(url)).convert('RGBA')
+    n_im = Image.alpha_composite(im2, im1)
+    date = str(datetime.date.today())
+
+    #Gerando texto da imagem
+    txt = Image.new('RGBA', n_im.size, (255, 255, 255, 0))
+
+    #fonte
+    fnt1 = ImageFont.truetype('Roboto-Regular.ttf', 15)
+
+    # gerando imagem
+    d = ImageDraw.Draw(txt)
+
+    # gerando texto
+    d.text((50, 495), f"data - {date}", font=fnt1, fill=(255, 255, 255, 255))
+    out = Image.alpha_composite(n_im, txt)
+
+    return out 
+
+# FUNÇÃO PARA CRIAÇÃO DE IMAGEM
+def make_music_image(url=str):
+    '''
+    Função retorna imagem de capa da música do dia
+    :param url: [STR] URL da imagem do Spotify que será usada como capa
+    :param linha_1: [STR] Linha 1 do título da playlist
+    :param linha_2: [STR] Linha 2 do título da playlist
+    :return PIL.Image.Image out:
+    '''
+
+    # gerando sobreposição da mascara com imagem do spotify
+    url = url
+    im1 = Image.open('musica.png').convert('RGBA')
+    im2 = Image.open(urlopen(url)).convert('RGBA')
+    n_im = Image.alpha_composite(im2, im1)
+    date = str(datetime.date.today())
+
+    #Gerando texto da imagem
+    txt = Image.new('RGBA', n_im.size, (255, 255, 255, 0))
+
+    #fonte
+    fnt1 = ImageFont.truetype('Roboto-Regular.ttf', 15)
+
+    # gerando imagem
+    d = ImageDraw.Draw(txt)
+
+    # gerando texto
+    d.text((50, 495), f"data - {date}", font=fnt1, fill=(255, 255, 255, 255))
+    out = Image.alpha_composite(n_im, txt)
+
+    return out 
+
+
 # FUNÇÕES PARA ENVIO DE INFORMAÇÕES PARA O TELEGRAM
 # Função para funcionamento do bot
 def bot_telegram(sheet1, sheet2, token=str, header_access=dict):
@@ -80,7 +177,8 @@ def bot_telegram(sheet1, sheet2, token=str, header_access=dict):
     if user_text == '/start':
         bot_text = f'''Olá, {user_name}, tudo bem? Boas vindas ao Weaver. O que você gostaria de fazer?
 /busca: Busca por algo específico no Spotify
-/novo: Recebe informações sobre o último lançamento de faixa no Brasil.'''
+/novo: Recebe informações sobre o último lançamento de faixa no Brasil
+/sugestao_musica: (Coloque aqui, em ordem e entre vírgulas, um artista que você gosta e um gênero de música que você curte)'''
     
     elif user_text == '/busca':
         bot_text = '''Você pode buscar por diversas coisas no spotify. Aqui está uma lista de comandos que você pode mandar para mim:
@@ -175,12 +273,36 @@ O nome da pessoa que fez essa playlist é {playlist_owner}.
         new_url = new_info['albums']['items'][0]['external_urls']['spotify']
         new_name = new_info['albums']['items'][0]['name']
         new_release = new_info['albums']['items'][0]['release_date']
-        new_artist = new_info['albums']['items'][0]['artists'][0]['name']
+        new_image_url = new_info['albums']['items'][0]['images'][0]
+        img = make_music_image(url=new_image_url)
+        for n in new_info['albums']['items'][0]['artists']:
+            new_artist.append(n['name'])
+            new_artist = ', '.join(new_artist)
+        bot_img = {"chat_id": user_id, 'photo': img}
+        requests.post(f"https://api.telegram.org./bot{token}/sendPhoto", data=bot_img).json()
         bot_text = f'''O mais novo lançamento no Brasil é {new_name}, de {new_artist}.
 O link do lançamento é: {new_url}
 Esse lançamento é um {new_type}.
 Foi lançado em {new_release}'''
-            
+
+    elif '/sugestao_musica' in user_mens:
+        user_input = user_mens.split(',').strip()
+        artist = user_input[0]
+        genres = user_input[1]
+        sugest = recommend_noti(header_access, [artist], [genres])
+        data = recommend_noti(header_access, artist, genres)
+        artist_name = data['artists'][0]['name']
+        music_name = data['name']
+        image_url = data['images'][0]['url']
+        music_url = data['tracks']['items'][0]['external_urls']['spotify']
+        img = make_music_image(url=image_url)
+        bot_text = f'''A sugestão de música que você pediu chegou!
+Para você que gosta de {artist} e {genres}, recomendo {music_name}, de {artist_name}!
+Acesse a música aqui: {music_url}        
+        '''
+        bot_img = {"chat_id": user_id, 'photo': img}
+        requests.post(f"https://api.telegram.org./bot{token}/sendPhoto", data=bot_img).json()
+
     else:
         bot_text = 'Desculpe, eu não entendi. Por favor, responda de acordo com as instruções de cada função!'
     
